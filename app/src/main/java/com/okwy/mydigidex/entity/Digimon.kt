ca8867f3153024e6335c682c52beae1f607b01b3
oknwachukwu <oknwachukwu@ecobank.com>
2022-03-06 10:06:11 +0100

    Architecture setup ongoing

---
+++ app/src/main/java/com/okwy/mydigidex/entity/Digimon.kt
@@ -0,0 +1,7 @@
+package com.okwy.mydigidex.entity
+
+data class Digimon(
+    val name : String = "",
+    val img : String = "",
+    val level : String = ""
+)
---
+++ app/src/main/java/com/okwy/mydigidex/views/Splash.kt
@@ -0,0 +1,12 @@
+package com.okwy.mydigidex.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.okwy.mydigidex.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
