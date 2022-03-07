3fb681bdddff3cd8ce6292465de9db8ce8b2eff5
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2022-03-07 16:18:23 +0100

    Network Bound Resource for database caching complete

--- app/src/main/java/com/okwy/mydigidex/util/NetworkBoundResource.kt
+++ app/src/main/java/com/okwy/mydigidex/util/NetworkBoundResource.kt
@@ -1,2 +1,38 @@
 package com.okwy.mydigidex.util
 
+import kotlinx.coroutines.flow.*
+
+inline fun <ResultType, RequestType> networkBoundResource(
+    crossinline query: () -> Flow<ResultType>,  //fetch data from db
+    crossinline fetch: suspend () -> RequestType,   //fetch data from rest api
+    crossinline saveFetchResult: suspend (RequestType) -> Unit,     //send data from rest api to db
+    crossinline shouldFetch: (ResultType) -> Boolean = { true }     //deciding whether to fetch new data from api or use cache
+
+) = flow {
+    //obtain one list of digimon from db
+    val data = query().first()
+
+    //obtain data to update cache where necessary
+    val flow = if(shouldFetch(data)){
+
+        //Loading the cache data
+        emit(Resource.Loading(data))
+        try {
+            //save data to cache
+            saveFetchResult(fetch())
+            //new data from the api
+            query().map { Resource.Success(it) }
+        }catch (err: Throwable){
+            //error then cache data
+            query().map{Resource.Error(it, err)}
+        }
+    }else{
+        //cache
+        query().map { Resource.Success(it) }
+    }
+
+
+    //get all
+    emitAll(flow)
+
+}
